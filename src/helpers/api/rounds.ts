91070c2ab8b56a110375212e8bcc812744d5f09a
Kirstin Buchanan <buchananmkirstin@gmail.com>
2021-11-01 18:32:57 +0000

    fix(api/rounds): fix switch statement

--- src/helpers/api/rounds.ts
+++ src/helpers/api/rounds.ts
@@ -1,20 +1,23 @@
-export const playGame = (playerGesture) => {
-  let computerGesture;
+export const playGame = ({ playerGesture }) => {
   let result;
+  let computerGesture;
 
   const n = Math.ceil(Math.random() * 3);
 
   switch (n) {
     case 1:
       computerGesture = 'rock';
+      break;
     case 2:
       computerGesture = 'paper';
+      break;
     case 3:
       computerGesture = 'scissors';
+      break;
   }
-
-  console.log('computerGesture', computerGesture);
-  console.log('playerGesture', playerGesture);
+  console.log('n', n);
+  console.log('player', playerGesture);
+  console.log('computergesture', computerGesture);
 
   if (playerGesture === 'rock' && computerGesture === 'rock') {
     result = 'draw!';
--- src/pages/api/rounds/index.tsx
+++ src/pages/api/rounds/index.tsx
@@ -3,7 +3,7 @@ import { playGame } from '../../../helpers/api/rounds';
 export default (req, res) => {
   const { gesture } = JSON.parse(req.body);
 
-  const result = playGame(gesture);
+  const result = playGame({ playerGesture: gesture });
 
   res.statusCode = 200;
   res.json({ result });
